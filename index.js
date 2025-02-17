969334803c7c9e19caaf030f041aec0535497f11
Emmanuel Howard <emmanuelhoward3000@gmail.com>
2025-02-17 11:55:47 -0500

    Learning Variables & Data Types

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-// cconsole.log("Hello World!");
-// cconsole.log("Hello World #2");
+// Print using this console.log() function
+console.log("Hello World!");
+console.log("Hello World #2");
 
-document.getElementById("myh1").textContent = "Hello!";
-document.getElementById("myP").textContent = "How are you?"
+// Pop up using this alert() function
+// document.getElementById("myh1").textContent = "Hello!";
+// document.getElementById("myP").textContent = "How are you?";
+
+// Variables: A decleration (let x) & an assignement (x=100)
+let age = 21;
+let salary = 10.99;
+console.log(age); // console.log is a function that prints to the console for the dev
+console.log(`You are ${age} years old and make ${salary} per hour`)
+console.log(typeof age); // Shows type
+
+
+// Data Types: Number, String, Boolean, Object, Array
+let num = 100; // Number
+let username = "John"; // String
+let isTrue = true; // Boolean
+let obj = {name: "John", age: 21}; // Object
+let arr = [10, 20, 30]; // Array
+
+document.getElementById("p1").textContent = username;
+document.getElementById("p2").textContent = `${username} is ${age} years old!`;
+document.getElementById("p3").textContent = salary;
