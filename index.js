865ea15012584cb55721097f546dd14601c86cf7
Emmanuel Howard <emmanuelhoward3000@gmail.com>
2025-02-17 11:27:00 -0500

    Learning baseic setup (connect, print, alerts & id's)

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-console.log("Hello World!");
-console.log("Hello World #2");
+// cconsole.log("Hello World!");
+// cconsole.log("Hello World #2");
+
+document.getElementById("myh1").textContent = "Hello!";
+document.getElementById("myP").textContent = "How are you?"
