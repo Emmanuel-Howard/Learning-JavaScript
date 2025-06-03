23f16c628da5ef8a54b8d04de4f6e519a6068596
Emmanuel-Howard <emmanuelhoward3000@gmail.com>
2025-06-02 20:06:02 -0700

    switch case statements

--- freecodecamp tutorial/tutorial.js
+++ freecodecamp tutorial/tutorial.js
@@ -1,4 +1,9 @@
-let name = 'Emmanuel';
-let age = 30;
+let hour = 10;
 
-console.log(`Hello, my name is ${name} and I am ${age} years old.`);
+if (hour < 6 && hour <= 12) {
+    console.log = "Good morning!";
+else if (hour >= 12 && hour < 18)
+    console.log("Good afternoon!");
+else
+    console.log("Good evening!");
+}
